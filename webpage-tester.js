b0bc80ae917b5457d393305b219f16457fa03f84
john8895 <paiching01@gmail.com>
2021-03-09 13:22:45 +0800

    update

--- webpage-tester.js
+++ webpage-tester.js
@@ -4,27 +4,24 @@
  * Date: 2021.03.08
  */
 
-/**
- * 1: 檢查 a 有無 title
- * 2: 檢查 img 有無 alt
- */
-window.onload = function () {
+(function () {
+    "use strict";
+
+    const promptStyleText = 'position:absolute;bottom:-20px;left:0;color:red;font-size:15px;font-weight:normal;'
+
+    checkLinkTitle();
+    checkImageAlt();
     checkElement();
+    // checkFilePath();
 
-    function checkElement() {
-        const promptStyleText = 'position:absolute;bottom:-20px;left:0;color:red;font-size:15px;font-weight:normal;'
+    function checkLinkTitle() {
+        const aTags = document.getElementsByTagName('a');
+        const aArray = [...aTags]
 
-        checkLinkTitle();
-        checkImageAlt();
-        checkElement();
-
-        function checkLinkTitle() {
-            const aTags = document.getElementsByTagName('a');
-            const aArray = [...aTags]
-
-            let aFilter = aArray.filter((item) => {
-                return !item.hasAttribute('title')
-            })
+        let aFilter = aArray.filter((item) => {
+            return !item.hasAttribute('title')
+        })
+        if (aFilter) {
             console.log('a total:', aFilter.length)
 
             aFilter.forEach((item, index) => {
@@ -33,18 +30,22 @@ window.onload = function () {
                 prompt.style.cssText = promptStyleText;
                 prompt.innerHTML = 'title';
                 item.appendChild(prompt);
-                console.log(index + 1, item)
+                // console.log(index + 1, item)
             })
         }
 
 
-        function checkImageAlt() {
-            const imgTags = document.getElementsByTagName('img');
-            const imgArray = [...imgTags]
+    }
 
-            let imgFilter = imgArray.filter((item) => {
-                return !item.hasAttribute('alt')
-            })
+
+    function checkImageAlt() {
+        const imgTags = document.getElementsByTagName('img');
+        const imgArray = [...imgTags]
+
+        let imgFilter = imgArray.filter((item) => {
+            return !item.hasAttribute('alt')
+        })
+        if (imgFilter) {
             console.log('img alt total:', imgFilter.length)
 
             imgFilter.forEach((item, index) => {
@@ -53,24 +54,29 @@ window.onload = function () {
                 prompt.style.cssText = promptStyleText;
                 prompt.innerHTML = 'alt';
                 item.appendChild(prompt);
-                console.log(index + 1, item)
+                // console.log(index + 1, item)
             })
         }
 
-
-        function checkElement() {
-            const editors = document.getElementsByClassName('webeditor');
-            const editorsEl = [...editors]
-
-            editorsEl.forEach((item) => {
-                item.style.cssText = 'backgroundColor:red;outline:2px solid deeppink;position:relative;';
-                const prompt = document.createElement('span');
-                prompt.style.cssText = promptStyleText + 'color:blue;';
-                prompt.innerHTML = 'webeditor';
-                item.appendChild(prompt);
-            })
-        }
     }
 
 
-}
+    function checkElement() {
+        const editors = document.getElementsByClassName('webeditor');
+        const editorsEl = [...editors]
+
+        editorsEl.forEach((item) => {
+            item.style.cssText = 'backgroundColor:red;outline:2px solid deeppink;position:relative;';
+            const prompt = document.createElement('span');
+            prompt.style.cssText = promptStyleText + 'color:blue;';
+            prompt.innerHTML = 'webeditor';
+            item.appendChild(prompt);
+        })
+    };
+
+
+    // function checkFilePath() {
+    //     const imgs = document.querySelectorAll('img')
+    //     console.log(imgs)
+    // }
+})();
